3d9f91c404d6f3c7288685610092b37a0ad9f2e4
dhruvsharmadrsh <dhruvsharmadrsh@gmail.com>
2026-02-03 17:57:44 +0530

    Style:solutions page fix

--- src/components/services/InteractiveValues.tsx
+++ src/components/services/InteractiveValues.tsx
@@ -121,7 +121,7 @@ const ValueCard = ({
                 />
             </motion.div>
 
-            {/* Content */}
+            {/* Content */ }
             <div className="relative flex flex-col p-7 md:p-8 text-white z-10 min-h-[420px]">
 
                 {/* Top Row */}
